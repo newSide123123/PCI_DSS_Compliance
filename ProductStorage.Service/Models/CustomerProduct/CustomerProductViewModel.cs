6aa33dfabbd7fd44491e3698c6307233522f8afd
newSide123123 <newSide01901@tutamail.com>
2023-09-27 14:16:48 +0300

    improve logging

---
+++ ProductStorage.Service/Models/CustomerProduct/CustomerProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.Service.Models.CustomerProduct
+{
+    public class CustomerProductViewModel
+    {
+        public string CustomerName { get; set; }  // Customer table
+        public string CustomerPhone { get; set; } // Customer table
+        public int OrderedAmount { get; set; }    // CustomerProduct table
+        public string ProductName { get; set; }   // Product table
+        public int TotalPrice { get; set; }       // CustomerProduct table field ("Amount") * Product table field ("Price")
+    }
+}
