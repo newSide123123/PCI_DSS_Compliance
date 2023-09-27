8cf0e7c7a9ea9dcc1dfaa7aa01228f583d07b9ad
newSide123123 <newSide01901@tutamail.com>
2023-09-27 14:16:48 +0300

    Add validation

---
+++ ProductStorage.Service/Models/Product/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductStorage.Service.Models.Product
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
