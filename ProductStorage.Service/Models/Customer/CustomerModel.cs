809d4df7759ce29cb2f7abc8b0df67a9324aa226
newSide123123 <newSide01901@tutamail.com>
2023-09-28 14:16:48 +0300

    update user guide

---
+++ ProductStorage.Service/Models/Customer/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductStorage.DAL.Entities;
+
+namespace ProductStorage.Service.Models.Customer
+{
+    public class CustomerModel
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
