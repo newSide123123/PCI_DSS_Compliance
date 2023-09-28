a348c15332f547fa7e14dda1bba1371ada6c8699
newSide123123 <newSide01901@tutamail.com>
2023-09-28 14:16:48 +0300

    fix css issue

---
+++ ProductStorage.Service/Models/CustomerProduct/CustomerProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProductStorage.Service.Models.CustomerProduct
+{
+    public class CustomerProductModel
+    {
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
