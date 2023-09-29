6c9a874716f0605ea53c66d9819f14faa5fcb3b4
newSide123123 <newSide01901@tutamail.com>
2023-09-29 14:16:48 +0300

    update server config

---
+++ ProductStorage.Service/Interfaces/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using ProductStorage.DAL.Entities;
+using ProductStorage.Service.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductStorage.Service.Models.Product;
+
+namespace ProductStorage.Service.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IBaseResponse<IEnumerable<ProductModel>>> GetProducts();
+
+        Task<IBaseResponse<ProductModel>> GetById(int id);
+
+        Task<IBaseResponse<ProductModel>> GetByName(string name);
+
+        Task<IBaseResponse<bool>> Delete(int id);
+
+        Task<IBaseResponse<bool>> Create(ProductModel customer);
+
+        Task<IBaseResponse<bool>> Update(int id, ProductModel newEntity);
+    }
+}
