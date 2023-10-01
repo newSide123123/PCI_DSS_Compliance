2753e4f96964e2cf6b4e3822616653fc0a73899c
newSide123123 <newSide01901@tutamail.com>
2023-10-01 14:16:48 +0300

    update config settings

---
+++ ProductStorage.Service/Enum/StatusCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.Service.Enum
+{
+    public enum StatusCode
+    {
+        EntityIsNull = 0,
+        // User doesn't exist in the collection
+        // Success
+        OK = 200,
+        // 0 items in the collection
+        ZeroItemsFound = 300,
+        // InternalServerError
+        InternalServerError = 500,
+
+        UserNotFound = 600,
+
+        ProductNotFound = 700,
+
+        CustomerProductNotFound = 800
+    }
+}
