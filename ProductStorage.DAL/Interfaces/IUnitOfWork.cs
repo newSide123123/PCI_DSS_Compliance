f38372100363cf3b8a8dbdfa52a3cd8da435a4f3
newSide123123 <newSide01901@tutamail.com>
2023-10-15 14:16:48 +0300

    update config settings

---
+++ ProductStorage.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ProductStorage.DAL.Entities;
+using ProductStorage.DAL.Repositories;
+using ProductStorage.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.DAL.Interfaces
+{
+    public interface IUnitOfWork 
+    {
+        ICustomerRepository Customers { get; }
+        IProductRepository Products { get; }
+        ICustomerProductRepository CustomerProducts { get;}
+    }
+}
