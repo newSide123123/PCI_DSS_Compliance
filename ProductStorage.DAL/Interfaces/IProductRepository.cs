95b4a560835389a19347a688d7c5ebb498b3fb03
newSide123123 <newSide01901@tutamail.com>
2023-10-15 14:16:48 +0300

    update npm packages

---
+++ ProductStorage.DAL/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductStorage.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.DAL.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+        Task<Product> GetByName(string name);
+        Task<Product> GetById(int id);
+
+        Task<bool> Update(int id, Product newEntity);
+    }
+}
