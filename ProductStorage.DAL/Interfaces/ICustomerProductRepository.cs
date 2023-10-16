dfd6818ff935d871fceea8d38d6a280a8521cb19
newSide123123 <newSide01901@tutamail.com>
2023-10-16 14:16:48 +0300

    Updated user model

---
+++ ProductStorage.DAL/Interfaces/ICustomerProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProductStorage.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.DAL.Interfaces
+{
+    public interface ICustomerProductRepository : IBaseRepository<CustomerProduct>
+    {
+        Task<CustomerProduct> GetByIds(int customerId, int productId);
+        Task<List<CustomerProduct>> GetByCustomerId(int id);
+
+        Task<List<CustomerProduct>> GetByProductId(int id);
+
+        Task<CustomerProduct> GetByAmount(int amount);
+
+        Task<bool> Update(int customerId, int productId, int newAmount);
+    }
+}
