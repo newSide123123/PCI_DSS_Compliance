ceef8722cbc8355ff6e2db0c3168cd0901e5f895
newSide123123 <newSide01901@tutamail.com>
2023-10-16 14:16:48 +0300

    add new route

---
+++ ProductStorage.DAL/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductStorage.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.DAL.Interfaces
+{
+    public interface ICustomerRepository : IBaseRepository<Customer>
+    {
+        Task<Customer> GetByName(string name);
+        Task<Customer> GetById(int id);
+
+        Task<bool> Update(int id, Customer newEntity);
+    }
+}
