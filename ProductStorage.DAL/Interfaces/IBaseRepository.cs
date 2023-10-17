1f8078c91d0403a71a8167492680e7f5170c2c87
newSide123123 <newSide01901@tutamail.com>
2023-10-17 14:16:48 +0300

    Fix issues with file uploads

---
+++ ProductStorage.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductStorage.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStorage.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<bool> Create(T entity);
+
+        Task<List<T>> Select();
+
+        Task<bool> Delete(T entity);
+
+    }
+}
