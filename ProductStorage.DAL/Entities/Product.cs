fcb5fe3c6e61715c19f7ab43cbd59f83b8ee448e
newSide123123 <newSide01901@tutamail.com>
2023-10-17 14:16:48 +0300

    fix bug in auth

---
+++ ProductStorage.DAL/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProductStorage.DAL.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+
+        public List<Customer> Customers { get; set; } // one product - many customers
+        public List<CustomerProduct> CustomerProducts { get; set; }
+    }
+}
