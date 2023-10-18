8774354fb3d72bb89851c996e5b8ee277b9044cb
newSide123123 <newSide01901@tutamail.com>
2023-10-18 14:16:48 +0300

    Implement OAuth2

---
+++ ProductStorage.DAL/Entities/CustomerProduct.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ProductStorage.DAL.Entities
+{
+    [Table("CustomersProducts")]
+    public  class CustomerProduct
+    {
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
