cb99c2dbfe7a0ac90068a823d21aca68ddfda39f
newSide123123 <newSide01901@tutamail.com>
2023-10-18 14:16:48 +0300

    improve logging

---
+++ ProductStorage.DAL/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+namespace ProductStorage.DAL.Entities
+{
+    public  class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public List<Product> Products { get; set; } // one customer - many products
+        public List<CustomerProduct> CustomerProducts { get; set; }
+    }
+}
