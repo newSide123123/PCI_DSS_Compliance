b3bf0acd0a707d987da4c21b24d95bb09ed59c5d
newSide123123 <newSide01901@tutamail.com>
2024-07-12 14:16:48 +0300

    Improve performance

---
+++ ConsoleOutput_WebApi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ConsoleOutput_WebApi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(@"https://localhost:44384/CustomerProduct/Select").Result;
+                var result = response.Content.ReadAsAsync<List<CustomerProductViewModel>>().Result;
+
+                foreach (CustomerProductViewModel item in result)
+                    Console.WriteLine($"{item.CustomerName} -- {item.CustomerPhone} -- {item.ProductName} - {item.TotalPrice}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
