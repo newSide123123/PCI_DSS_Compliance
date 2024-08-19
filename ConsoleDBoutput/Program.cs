dce151c6564cadb5f3f93d00fd534ba926b1c3f9
newSide123123 <newSide01901@tutamail.com>
2024-08-19 14:16:48 +0300

    update translations

---
+++ ConsoleDBoutput/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProductStorage.DAL.EF;
+using ProductStorage.DAL.Entities;
+using System;
+using System.Linq;
+
+namespace ConsoleDBoutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var db = new Context())
+            //{
+
+            //    var query = from b in db.Customers
+            //                orderby b.Name
+            //                select b;
+
+            //    Console.WriteLine("All customers in the database:");
+            //    foreach (var item in query)
+            //    {
+            //        Console.WriteLine(item.Name);
+            //    }
+
+
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
